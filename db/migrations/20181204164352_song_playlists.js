e22d1e902af77f2c5c3837ef8b22d64f32cf5ff8
ClaireBvs <clairembeauvais@gmail.com>
2018-12-04 16:48:36 -0700

    Add join table song_playlists

---
+++ db/migrations/20181204164352_song_playlists.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('song_playlists', function(table) {
+      table.integer('playlist_id').unsigned()
+      table.foreign('playlist_id')
+        .references('playlists.id');
+      table.integer('favorite_id').unsigned()
+      table.foreign('favorite_id')
+        .references('favorites.id');
+
+      table.timestamps(true, true);
+    }),
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('song_playlists')
+  ]);
+};
