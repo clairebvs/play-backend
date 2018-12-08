319fde55a711efd2b9551c8c528118bf82edaf7b
ClaireBvs <clairembeauvais@gmail.com>
2018-12-08 15:56:37 -0700

    Refactor test GET api/v1/playlists/:id/songs [#16]

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -113,27 +113,24 @@ describe("My API routes", () => {
 
   describe("GET /api/v1/playlists/:id/songs", () => {
     it("should return one playlist by id and the songs associated", done => {
-      database('playlists').select('*').then(data => resolve(data))
-      function resolve(playlist) {
-        chai.request(server)
-        .get(`/api/v1/playlists/${playlist[0].id}`)
-        .end((err, response) => {
-          response.should.have.status(200);
-          response.should.be.json;
-          response.body.should.be.a('array');
-          response.body.length.should.equal(1);
-          response.body[0].should.have.property('id');
-          response.body[0].should.have.property('playlist_name');
-          response.body[0].should.have.property('songs');
-          response.body[0].playlist_name.should.equal('Birthday Songs');
-          response.body[0].songs[0].name.should.equal('Happy Birthday');
-          response.body[0].songs[0].artist_name.should.equal('Becca and Claire');
-          response.body[0].songs[0].genre.should.equal('Pop');
-          response.body[0].songs[0].song_rating.should.equal('100');
-          done();
-        });
-      };
-    });
+      chai.request(server)
+      .get(`/api/v1/playlists/1/songs`)
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(1);
+        response.body[0].should.have.property('id');
+        response.body[0].should.have.property('playlist_name');
+        response.body[0].should.have.property('songs');
+        response.body[0].playlist_name.should.equal('Birthday Songs');
+        response.body[0].songs[0].name.should.equal('Happy Birthday');
+        response.body[0].songs[0].artist_name.should.equal('Becca and Claire');
+        response.body[0].songs[0].genre.should.equal('Pop');
+        response.body[0].songs[0].song_rating.should.equal('100');
+        done();
+      });
+  });
   });
 
   describe("POST /api/v1/favorites", () => {
