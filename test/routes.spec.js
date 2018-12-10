f5fae87c7d97885f8fba7bbfaab7a6072489e399
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-12-10 11:46:12 -0700

    return tests to original URL calling row with id 1 to reduce complexity

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -52,27 +52,24 @@ describe("My API routes", () => {
 
   describe("GET /api/v1/favorites/:id", () => {
     it("should return a favorite by id", done => {
-      database('favorites').select('*').then(data => resolve(data))
-      function resolve(favorite) {
-        chai.request(server)
-        .get(`/api/v1/favorites/${favorite[0].id}`)
-        .end((err, response) => {
-          response.should.have.status(200);
-          response.should.be.json;
-          response.body.should.be.a('array');
-          response.body.length.should.equal(1);
-          response.body[0].should.have.property('id');
-          response.body[0].should.have.property('name');
-          response.body[0].should.have.property('artist_name');
-          response.body[0].should.have.property('genre');
-          response.body[0].should.have.property('song_rating');
-          response.body[0].name.should.equal('Happy Birthday');
-          response.body[0].artist_name.should.equal('Becca and Claire');
-          response.body[0].genre.should.equal('Pop');
-          response.body[0].song_rating.should.equal('100');
-          done();
-        });
-      };
+      chai.request(server)
+      .get(`/api/v1/favorites/1`)
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(1);
+        response.body[0].should.have.property('id');
+        response.body[0].should.have.property('name');
+        response.body[0].should.have.property('artist_name');
+        response.body[0].should.have.property('genre');
+        response.body[0].should.have.property('song_rating');
+        response.body[0].name.should.equal('Happy Birthday');
+        response.body[0].artist_name.should.equal('Becca and Claire');
+        response.body[0].genre.should.equal('Pop');
+        response.body[0].song_rating.should.equal('100');
+        done();
+      });
     });
   });
 
@@ -142,21 +139,18 @@ describe("My API routes", () => {
 
   describe("GET /api/v1/playlists/:id", () => {
     it("should return one playlist by id", done => {
-      database('playlists').select('*').then(data => resolve(data))
-      function resolve(playlist) {
-        chai.request(server)
-        .get(`/api/v1/playlists/${playlist[0].id}`)
-        .end((err, response) => {
-          response.should.have.status(200);
-          response.should.be.json;
-          response.body.should.be.a('array');
-          response.body.length.should.equal(1);
-          response.body[0].should.have.property('id');
-          response.body[0].should.have.property('playlist_name');
-          response.body[0].playlist_name.should.equal('Birthday Songs');
-          done();
-        });
-      };
+      chai.request(server)
+      .get(`/api/v1/playlists/1`)
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(1);
+        response.body[0].should.have.property('id');
+        response.body[0].should.have.property('playlist_name');
+        response.body[0].playlist_name.should.equal('Birthday Songs');
+        done();
+      });
     });
   });
 
