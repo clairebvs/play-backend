8bb09686bac987f68808decadfdefa32157e39c0
ClaireBvs <clairembeauvais@gmail.com>
2018-12-10 06:16:40 -0700

    Add more contents to seed and for testing [#22]

---
+++ db/seeds/dev/song_playlists.js
@@ -0,0 +1,15 @@
+exports.seed = function(knex, Promise) {
+  return knex('song_playlists').del()
+    .then(() => {
+      return Promise.all([
+        knex('song_playlists').insert([
+          {favorite_id: 2, playlist_id: 1},
+          {favorite_id: 1, playlist_id: 1},
+          {favorite_id: 3, playlist_id: 2}
+        ])
+      .then(() => console.log('Seeding complete!'))
+      .catch(error => console.log(`Error seeding data: ${error}`))
+      ])
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
